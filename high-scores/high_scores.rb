c94a96158e017ad0d3128559ca2b31335f559e57
poacosta <poacosta87@gmail.com>
2022-04-29 22:14:54 -0500

    Adding solution for High Scores

---
+++ high-scores/high_scores.rb
@@ -0,0 +1,23 @@
+class HighScores
+  attr_reader :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def latest
+    @scores.last
+  end
+
+  def personal_top_three
+    @scores.sort.reverse.take(3)
+  end
+
+  def latest_is_personal_best?
+    latest == personal_best
+  end
+end
