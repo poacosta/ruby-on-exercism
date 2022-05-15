0316ed56ea91fcdf675064b29020a1b8225c6388
poacosta <poacosta87@gmail.com>
2022-05-14 22:26:45 -0500

    Difference Of Squares

---
+++ difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Squares
+  def initialize(number)
+    @number = number
+  end
+
+  def square_of_sum
+    ((@number * (@number + 1)) / 2) ** 2
+  end
+
+  def sum_of_squares
+    (@number * (@number + 1) * (2 * @number + 1)) / 6
+  end
+
+  def difference
+    (square_of_sum - sum_of_squares).abs
+  end
+end
