6c467e5d01161f0d63b6c284175292760bcea319
Pedro Orlando Acosta Pereira <poacosta87@gmail.com>
2023-07-03 22:45:35 -0600

    Refactoring Diamond

--- diamond/diamond.rb
+++ diamond/diamond.rb
@@ -1,23 +1,28 @@
+# frozen_string_literal: true
+
+# Diamond
 class Diamond
+  ALPHABET = ('A'..'Z').to_a.freeze
+
   def self.make_diamond(letter)
     return "A\n" if letter == 'A'
 
-    alphabet = ('A'..'Z').to_a
-    index = alphabet.index(letter)
+    index = ALPHABET.index(letter)
     diamond = []
-    spikes(diamond, index)
-    (1..index).each { |i| line(diamond, index, i, alphabet) }
-    (1..index - 1).reverse_each { |i| line(diamond, index, i, alphabet) }
-    spikes(diamond, index)
+    spike(diamond, index)
+    (1..index).each { |i| line(diamond, index, i) }
+    (1..index - 1).reverse_each { |i| line(diamond, index, i) }
+    spike(diamond, index)
     compose(diamond)
   end
 
-  def self.spikes(diamond, index)
+  def self.spike(diamond, index)
     diamond << "#{' ' * index}A#{' ' * index}"
   end
 
-  def self.line(diamond, index, i, alphabet)
-    diamond << ' ' * (index - i) + alphabet[i] + ' ' * (2 * i - 1) + alphabet[i] + ' ' * (index - i)
+  def self.line(diamond, index, idx)
+    letter = ALPHABET[idx]
+    diamond << "#{' ' * (index - idx)}#{letter}#{' ' * (2 * idx - 1)}#{letter}#{' ' * (index - idx)}"
   end
 
   def self.compose(diamond)
